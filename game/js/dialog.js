e969b943a399175570a44a99c2d0bd406ebcdb51
Bruno Croci <crocidb@gmail.com>
2019-09-05 23:03:34 +0100

    basic dialog and choice system working

--- game/js/dialog.js
+++ game/js/dialog.js
@@ -5,34 +5,51 @@ class Dialog {
         this.dialogAuthor = qSelA("#dialog .author")[0];
         this.choiceElement = gId("choice");
         this.choiceText = [ gId("c1"), gId("c2") ];
+        this.choiceText.forEach((e) => e.addEventListener("click", this.clickChoice.bind(this, e)));
         this.callback = null;
         this.active = false;
         this.finished = false;
-        this.clickd = this.click.bind(this);
+        this.keep = false;
+        document.body.addEventListener('click', this.click.bind(this));
+        
+        this.author = "";
     }
 
-    showList(messages, author, callback) {
+    showList(messages, author, callback, keep) {
+        this.keep = keep;
         this.list = messages;
+        this.author = author;
         this.currentInList = 0;
 
         this.callback = callback;
 
         playaudio(SOUNDS.dialog_open);
-        document.body.addEventListener('click', this.clickd);
         
-        this._show(messages[this.currentInList], author);
+        this._show(messages[this.currentInList]);
     }
     
-    _show(message, author) {
+    show(message, author, callback, keep) {
+        this.keep = keep;
+        this.author = author;
+        this.callback = callback;
+        this.list = null;
+        this.currentInList = -1;
+        this._show(message);
+        
+        playaudio(SOUNDS.dialog_open);
+    }
+    
+    _show(message) {
         this.dialogElement.classList.remove("hidden");
 
-        console.log(this.dialogText);
+        console.log(message);
         this.dialogAuthor.innerHTML = "";
         
         // appearing effect
         let total = message.length;
-        let step = Math.ceil(total / 30);
+        let step = Math.ceil(total / 50);
         let that = this;
+
         co(function*() {
             let c = 0;
             while (c < total) {
@@ -42,40 +59,49 @@ class Dialog {
                 yield .05;
             }
             that.dialogText.innerHTML = message;
-            that.dialogAuthor.innerHTML = "&#8212; " + author;
+            that.dialogAuthor.innerHTML = "&#8212; " + that.author;
             that.finished = true;
         });
         
         this.active = true;
     }
     
-    show(message, author, callback) {
-        this.callback = callback;
-        this.list = null;
-        this.currentInList = -1;
-        this._show(message, author);
-        document.body.addEventListener('click', this.clickd);
-        
-        playaudio(SOUNDS.dialog_open);
-    }
-    
     close() {
         playaudio(SOUNDS.dialog_close);
         this.dialogElement.classList.add("hidden");
-        this.callback();
         this.finished = false;
-        document.body.removeEventListener('click', this.clickd);
+    }
+
+    showChoice(choices) {
+        for (let i = 0; i < choices.length; i++) {
+            this.choiceText[i].innerHTML = choices[i].t;
+            this.choiceText[i].callback = choices[i].c;
+        }
+
+        this.choiceElement.classList.remove("hidden");
+    }
+    
+    closeChoice() {
+        this.choiceElement.classList.add("hidden");
+    }
+
+    clickChoice(e) {
+        this.closeChoice();
+        this.close();
+
+        e.callback();
     }
 
     click() {
         if (this.active && this.finished) {
+            this.finished = false;
             if (this.list != null && this.currentInList > -1 && this.currentInList < this.list.length - 1) {
                 playaudio(SOUNDS.dialog_confirm);
                 this.currentInList++;
-                this.finished = false;
                 this._show(this.list[this.currentInList]);
             } else {
-                this.close();
+                if (!this.keep) this.close();
+                this.callback();
             }
         }
     }
--- game/js/main.js
+++ game/js/main.js
@@ -1,5 +1,14 @@
 let dialog = new Dialog();
 
 (function() {
-    dialog.show("Test", "Daisy Walker", () => {});
+    dialog.showList([
+        "I am very excited today. I really love books, you know?",
+        "And got two new arrivals today in a large crate this morning. I am bringing them with me to The Library.",
+        "I know it will be a real busy day, but I hope to have enough time to check them out.",
+    ], "Daisy Walker", () => {
+        dialog.showChoice([
+            { t: "Oh! What are their titles?", c: () => {} },
+            { t: "I am not really into books, I am sorry, Mrs.", c: () => {} }
+        ]);
+    }, true);
 })();
